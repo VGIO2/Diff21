fa8201c9a0f33fca2d9dcc68d91b9f2e44b29d1f
Giovanna <orurog@gmail.com>
2016-12-23 12:10:25 -0500

    en proceso

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+function calcular()
+{
+    
+    var n= document.getElementById("n").value;
+    var psalida = document.getElementById("salida").value;
+    var diff= document.getElementById()
+  
+     
+
+    if (n>21) {
+            num21=21;
+            pSalida= parseInt(n)-num21;
+            
+        } 
+        
+        else if (n>21) {
+            pSalida.innerHTML = (parseInt(n)-num21)*2 ;
+        }
+        
+        else {
+            pSalida.innerHTML = ;
+        }
+    
+}
