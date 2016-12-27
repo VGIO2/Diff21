1c3a47334bd8fb0da458134166b70f4df9029724
Giovanna <orurog@gmail.com>
2016-12-27 11:40:17 -0500

    js terminado

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
-
-function calcular()
-{
-    
-    var n= document.getElementById("n").value;
-    var psalida = document.getElementById("salida").value;
-    var diff= document.getElementById()
   
-     
-
-    if (n>21) {
-            num21=21;
-            pSalida= parseInt(n)-num21;
-            
-        } 
-        
-        else if (n>21) {
-            pSalida.innerHTML = (parseInt(n)-num21)*2 ;
-        }
-        
-        else {
-            pSalida.innerHTML = ;
-        }
+function calcular(){
+    var n= document.getElementById("n");
+    var psalida = document.getElementById("salida");
+    var diff;
     
+    var varn=n.value;
+
+if(varn<21){
+    diff=21-varn;
+    psalida.innerHTML=diff;
+    
+}
+else{
+    diff=(varn-21)*2;
+    psalida.innerHTML=diff;
+}
 }
